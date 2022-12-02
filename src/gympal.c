e480b46e4783008b420bc840a54d25b47489b3cf
Andrew Lalis <andrewlalisofficial@gmail.com>
2022-12-02 11:45:33 +0100

    Added blink script.

--- src/gympal.c
+++ src/gympal.c
@@ -1,15 +1,19 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+void updatePin12(uint8_t status) {
+    PORTB = status << PORTB4;
+}
+
 int main() {
     // Pin 12 == Port B4, aka 4th bit on the B register.
     // Set pin 12 as output.
     DDRB = 1 << PORTB4;
-    // Set pin 12 as HIGH.
-    PORTB = 1 << PORTB4;
-    // Delay for 1 second.
-    _delay_ms(1000.0);
-    // Set pin 12 as LOW.
-    PORTB = 0 << PORTB4;
+    uint8_t ledOn = 1;
+    while (1) {
+        updatePin12(ledOn);
+        ledOn = !ledOn;
+        _delay_ms(1000.0);
+    }
     return 0;
 }
